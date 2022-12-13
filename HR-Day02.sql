537d3ab7ed6aae92878dcf2ce03ae4872f3b8ed9
dobby8282 <dobbyisfree8282@gmail.com>
2022-12-13 23:57:01 +0900

    Update HR-Day02.sql
    1

--- HR-Day02.sql
+++ HR-Day02.sql
@@ -1,1 +1,12 @@
--- test1
+SELECT
+    employees.employee_id,
+    employees.first_name,
+    employees.last_name,
+    employees.email,
+    employees.phone_number,
+    employees.hire_date,
+    employees.job_id,
+    employees.salary,
+    employees.commission_pct
+FROM
+    employees
